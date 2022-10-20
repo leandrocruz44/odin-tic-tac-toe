21900be3d8f77f425ea27ecea8fbd9dac302dcb6
leandrocruz <leandro.dinamo@gmail.com>
2022-10-20 04:58:39 -0300

    Release version
    HTML
    *Create a field for the player's name and their symbols
    *Create a start button and a restart button
    
    CSS
    *Apply style to the elements on the page
    *Create style classes that are activated when needed
    
    JS
    *Create a function that ends the game when a player gets a 3-in-a-row
    sequence
    *Create a function that clears the board
    *In the 'player' function, if the player do not fill the name fields,
    it shows "Player One" and "Player Two" on the fields
    *Create a function that restart the current game
    *Create a function that starts the game
    *Create a function that determines the match's winner or if it's a tie

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # odin-tic-tac-toe
 A project for the Web Development course by The Odin Project (Tic Tac Toe)
+
+Favicon from https://freesvg.org/1527091757
+
+Image https://freesvg.org/simplified-crown
+Image https://freesvg.org/zeimusu-trefoil-knot
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,8 @@
 const gameBoard = (function() {
-    let board = [null, null, null, null, null, null, null, null, null];
+    let board = ['', '', '', 
+                 '', '', '', 
+                 '', '', ''];
+
     function renderSymbols() {
         let fields = document.querySelectorAll('.field')
         fields.forEach((field) => {
@@ -10,15 +13,78 @@ const gameBoard = (function() {
             }
         })
     }
+
+    function restart() {
+        for (i = 0; i < board.length; i++){
+            board[i] = ''
+        }
+    }
+
+    function endGame() {
+        const gameboard = document.querySelector('#container')
+        if ((board[0] == 'X') && (board[1] == 'X') && (board[2] == 'X')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('X')
+        } else if ((board[3] == 'X') && (board[4] == 'X') && (board[5] == 'X')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('X')
+        } else if ((board[6] == 'X') && (board[7] == 'X') && (board[8] == 'X')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('X')
+        } else if ((board[0] == 'X') && (board[3] == 'X') && (board[6] == 'X')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('X')
+        } else if ((board[1] == 'X') && (board[4] == 'X') && (board[7] == 'X')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('X')
+        } else if ((board[2] == 'X') && (board[5] == 'X') && (board[8] == 'X')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('X')
+        } else if ((board[0] == 'X') && (board[4] == 'X') && (board[8] == 'X')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('X')
+        } else if ((board[2] == 'X') && (board[4] == 'X') && (board[6] == 'X')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('X')
+        } else if ((board[0] == 'O') && (board[1] == 'O') && (board[2] == 'O')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('O')
+        } else if ((board[3] == 'O') && (board[4] == 'O') && (board[5] == 'O')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('O')
+        } else if ((board[6] == 'O') && (board[7] == 'O') && (board[8] == 'O')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('O')
+        } else if ((board[0] == 'O') && (board[3] == 'O') && (board[6] == 'O')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('O')
+        } else if ((board[1] == 'O') && (board[4] == 'O') && (board[7] == 'O')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('O')
+        } else if ((board[2] == 'O') && (board[5] == 'O') && (board[8] == 'O')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('O')
+        } else if ((board[0] == 'O') && (board[4] == 'O') && (board[8] == 'O')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('O')
+        } else if ((board[2] == 'O') && (board[4] == 'O') && (board[6] == 'O')){
+            gameboard.style.pointerEvents = 'none';
+            winner.winnerUser('O')
+        } else if (board.includes('') == false){
+            winner.winnerUser('tie')
+            gameboard.style.pointerEvents = 'none';
+        }
+    }
+
     const addX = (x) => {
-        if (board[x] != null) {
+        if (board[x] != '') {
             alert('Invalid move')
         } else {
             board[x] = 'X'
         }
     }
     const addO = (x) => {
-        if (board[x] != null) {
+        if (board[x] != '') {
             alert('Invalid move')
         } else {
             board[x] = 'O'
@@ -26,12 +92,17 @@ const gameBoard = (function() {
     }
 
 
-    return {addX, addO, renderSymbols, board}
+    return {addX, addO, renderSymbols, endGame, restart}
 })();
 
 
 const player = function(name, id) {
-    const playerName = name || 'Mysterious Player'
+    let playerName = name
+    if (playerName == '' && id == 'playerOne') {
+        playerName = 'Player One'
+    } else if (playerName == '' && id == 'playerTwo') {
+        playerName = 'Player Two'
+    }
     const play = (x) => {
         if (id == 'playerOne'){
             gameBoard.addX(x)
@@ -44,26 +115,85 @@ const player = function(name, id) {
 }
 
 
-const game = function() {
+
+const game = function(name1, name2) {
     const fields = document.querySelectorAll('.field');
-    const lindo = player('Lindo', 'playerOne');
-    const feio = player('Feio', 'playerTwo');
-    let lindoActive = true
-    let feioActive = false
+    const playerOne = player(name1, 'playerOne');
+    const playerTwo = player(name2, 'playerTwo');
+    let playerOneActive = false
+    let playerTwoActive = true
     fields.forEach((field) => {
         field.addEventListener('click', () => {
-            if (lindoActive === true && field.innerText == '') {
-                lindoActive = false
-                feioActive = true
-            } else if (lindoActive === false && field.innerText == '') {
-                lindoActive = true
-                feioActive = false
+            if (playerOneActive === false && field.innerText == '') {
+                playerOneActive = true
+                playerTwoActive = false
+            } else if (playerOneActive === true && field.innerText == '') {
+                playerOneActive = false
+                playerTwoActive = true
             }
-            lindoActive ? lindo.play(field.id) : feio.play(field.id)
-            console.log(gameBoard.board)
+            playerOneActive ? playerOne.play(field.id) : playerTwo.play(field.id)
             gameBoard.renderSymbols()
+            gameBoard.endGame()
         })
-    })
+    }) 
+
+    // To restart the current game
+    const restart = (() => {
+        const restart = document.querySelector('#restart');
+        const gameboard = document.querySelector('#container')
+        restart.addEventListener('click', () => {
+            gameBoard.restart()
+            gameBoard.renderSymbols()
+            playerOneActive = false;
+            playerTwoActive = true;
+            gameboard.style.pointerEvents = 'all';
+            winner.restartWinner()
+        })
+    })();
 }
 
-game()         
+const begin = (() => {
+    const start = document.querySelector('#start');
+    const restart = document.querySelector('#restart');
+    const field = document.querySelectorAll('.inactiveField')
+    const playerOneFieldName = document.querySelector('#playerOneFieldName')
+    const playerTwoFieldName = document.querySelector('#playerTwoFieldName')
+    start.addEventListener('click', () => {
+        let nameX = document.getElementById('playerOne').value
+        let nameO = document.getElementById('playerTwo').value
+        const playerOne = player(nameX, 'playerOne')
+        const playerTwo = player(nameO, 'playerTwo')
+        playerOneFieldName.innerText = playerOne.playerName
+        playerTwoFieldName.innerText = playerTwo.playerName
+        start.classList.replace('btn', 'hide')
+        restart.classList.replace('hide', 'btn')
+        for (let i = 0; i < field.length; i++){
+            field[i].classList.replace('inactiveField', 'field')
+        }
+        game(nameX, nameO)
+    })   
+})();
+
+const winner = (() => {
+    const winnerPlayerOne = document.querySelector('#winnerOne');
+    const winnerPlayerTwo = document.querySelector('#winnerTwo');
+    function winnerUser(x) {
+        if (x === 'X') {
+            winnerPlayerOne.classList.replace('winnerOff', 'winnerOn')
+        } else if (x === 'O') {
+            winnerPlayerTwo.classList.replace('winnerOff', 'winnerOn')
+        } else if (x === 'tie') {
+            winnerPlayerTwo.classList.replace('winnerOff', 'tie')
+            winnerPlayerOne.classList.replace('winnerOff', 'tie')
+        }
+    }
+
+    function restartWinner() {
+        winnerPlayerOne.classList.replace('winnerOn', 'winnerOff')
+        winnerPlayerTwo.classList.replace('winnerOn', 'winnerOff')
+        winnerPlayerOne.classList.replace('tie', 'winnerOff')
+        winnerPlayerTwo.classList.replace('tie', 'winnerOff')
+    }
+
+    return {winnerUser, restartWinner}
+})()
